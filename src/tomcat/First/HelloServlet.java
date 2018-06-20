4eb5ac4c2007b23b84be552b060d37b06a44a6f1
lymboy <alayama@foxmail.com>
2018-06-20 23:27:54 +0800

    tomcat

---
+++ src/tomcat/First/HelloServlet.java
@@ -0,0 +1,27 @@
+package tomcat.First;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException 
+	{
+		response.setContentType("text/html;charset=utf-8");
+		response.getWriter().write("This a a test. Current time: " + new Date());
+	}
+
+}
